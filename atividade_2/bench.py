2963d200fe47219ce59c2ca1cb5d3144b6701128
Max <karkinhowo@gmail.com>
2025-10-03 17:04:53 -0300

    Python Analisys Done

--- atividade_2/bench.py
+++ atividade_2/bench.py
@@ -3,17 +3,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def main():
-    data = pd.read_csv( "./log_1.csv",  names=['MouseX' , 'MouseY' , 'LeftMouseButton' , 'RightMouseButton' , 'DrawTime' , 'ActionTime' , "NumberOfPoints" , 'MunberOfSides' ] )
-    drawTimeNumberOfPointsData = data.groupby( 'NumberOfPoints' , as_index=False )['DrawTime'].mean()
+    data = pd.read_csv( "./log_1.csv",  names=['MouseX' , 'MouseY' , 'LeftMouseButton' , 'RightMouseButton' , 'DrawTime' , 'ActionTime' , 'VoronoiDelaunayTime' , "NumberOfPoints" , 'MunberOfSides' ] )
+    drawTimeNumberOfPointsData = data.groupby( 'NumberOfPoints' , as_index=False )['VoronoiDelaunayTime'].mean()
     DrawTimeNumberOfPoints( drawTimeNumberOfPointsData )
 
 
 def DrawTimeNumberOfPoints( data ):
     
-    plt.plot( data['NumberOfPoints'] , data['DrawTime'] )
+    plt.plot( data['NumberOfPoints'] , data['VoronoiDelaunayTime'] )
     plt.xlabel("Number Of Points")
     plt.ylabel("Time To Draw (ms)")
-    plt.title("Time To Draw Points On Screen In Relation To Quantity")
+    plt.title("Time To Compute Voronoi And Delaunay In Relation To Quantity Of Points")
     plt.legend()
     plt.grid( True )
     plt.savefig( "graph.png" );
