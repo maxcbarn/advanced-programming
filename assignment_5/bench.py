6e255ad51c15f276013dfe21460cae8defb67677
Max <karkinhowo@gmail.com>
2025-11-07 17:06:51 -0300

    Data Analysis Done

--- assignment_5/bench.py
+++ assignment_5/bench.py
@@ -4,26 +4,80 @@ import matplotlib.pyplot as plt
 from scipy.interpolate import make_interp_spline
 
 def main():
-    data = pd.read_csv( "./logbenchmark.csv",  names=[  'ExecutionTime' , 'VertexQuantity' , 'VertexQuantityRobot' , 'PolyQuantity'] )
-    drawTimeNumberOfPointsData = data.groupby( 'VertexQuantity' )['ExecutionTime'].mean().reset_index()
-    DrawTimeNumberOfPoints( drawTimeNumberOfPointsData )
+    dataTime = pd.read_csv("./logs/TimeLog.csv", names=['Size', 'PathQuantity', 'ExecutionTime', 'Dist', 'PathBlocked' ])
+    dataTimeGrupped = dataTime.groupby(['Size', 'PathQuantity'])['ExecutionTime'].mean().reset_index()
+    df40 = dataTimeGrupped[ dataTimeGrupped['Size'] == 40 ]
+    df40 = df40.groupby('PathQuantity')['ExecutionTime'].mean().reset_index()
 
-def DrawTimeNumberOfPoints( data ):
+    TimeXQuantity( dataTimeGrupped )
+
+    dataObstacle = pd.read_csv("./logs/ObstacleLog.csv", names=[ 'PathQuantity', 'ObstacleQuantity' , 'ExecutionTime', 'Dist', 'PathBlocked' ])
+    dataObstacleGrupped = dataObstacle.groupby(['ObstacleQuantity', 'PathQuantity'])['ExecutionTime'].mean().reset_index()
+
+    ObstacleXTime( dataObstacleGrupped , df40 )
     
-    x_smooth = np.linspace(data['ExecutionTime'].min(), data['ExecutionTime'].max(), 300)
-    spl = make_interp_spline(data['ExecutionTime'], data['VertexQuantity'], k=3)  # k=3 → cúbica
-    y_smooth = spl(x_smooth)
+    dataDist = pd.read_csv("./logs/DistLog.csv", names=[ 'PathQuantity', 'ExecutionTime', 'Dist', 'PathBlocked' ])
+    dataDistGrupped = dataDist.groupby('Dist')['ExecutionTime'].mean().reset_index()
+    
+    TimeXDist( dataDistGrupped )
 
-    # Plotar linha suave
-    plt.plot(  y_smooth , x_smooth)
-    plt.xlabel("Number Of Vertex")
+def ObstacleXTime( data , df40 ):
+    data = data[ data['PathQuantity'] == 20 ]
+    data = data.groupby('PathQuantity')['ExecutionTime'].mean().reset_index()
+    
+    plt.plot(df40['PathQuantity'], df40['ExecutionTime'], label='Without Obstacle')
+    plt.plot(data['PathQuantity'], data['ExecutionTime'] ,label='With Obstacle')
+    
+    plt.xlabel("Quantity of Paths")
     plt.ylabel("Time To Compute (ms)")
-    plt.title("Time To Compute The Minkowski Sum In Relation To Vertex Quantity Of The Polygons")
+    plt.title("Time To Compute The Paths In Relation To The Quantity Of Obstacle Being 8 Times More Than Paths, And Grid Size 40x40")
     plt.legend()
-    plt.grid( True )
+    plt.grid(True)
     plt.tight_layout()
-    plt.savefig( "timeXvertex.png" , bbox_inches='tight' );
+    plt.savefig( "timeXobstacle.png" , bbox_inches='tight' );
+    plt.clf()
+
+def TimeXQuantity( data ):
+    
+    #x_smooth = np.linspace(data['ExecutionTime'].min(), data['ExecutionTime'].max(), 300)
+    #spl = make_interp_spline(data['ExecutionTime'], data['PathQuantity'], k=3)  # k=3 → cúbica
+    #y_smooth = spl(x_smooth)
+
+    df20 = data[ data['Size'] == 20 ]
+    df20 = df20.groupby('PathQuantity')['ExecutionTime'].mean().reset_index()
+    df40 = data[ data['Size'] == 40 ]
+    df40 = df40.groupby('PathQuantity')['ExecutionTime'].mean().reset_index()
+    
+    # Plotar linha suave
+    plt.plot(df20[ 'PathQuantity' ], df20['ExecutionTime'], label='Grid Size 20x20')
+    plt.plot(df40[ 'PathQuantity' ], df40['ExecutionTime'], label='Grid Size 40x40')
+    
+    plt.xlabel("Quantity of Paths")
+    plt.ylabel("Time To Compute (ms)")
+    plt.title("Time To Compute The Paths In Relation To The Path Quantity And Grid Size")
+    plt.legend()
+    plt.grid(True)
+    plt.tight_layout()
+    plt.savefig( "timeXquantity.png" , bbox_inches='tight' );
+    plt.clf()
 
 
+def TimeXDist( data ):
+    
+    #x_smooth = np.linspace(data['ExecutionTime'].min(), data['ExecutionTime'].max(), 300)
+    #spl = make_interp_spline(data['ExecutionTime'], data['PathQuantity'], k=3)  # k=3 → cúbica
+    #y_smooth = spl(x_smooth)
+    
+    # Plotar linha suave
+    plt.plot(data[ 'Dist' ], data['ExecutionTime'])
+    
+    plt.xlabel("Path Distance")
+    plt.ylabel("Time To Compute (ms)")
+    plt.title("Time To Compute The Paths In Relation To The Path Distance Grid Size 30x30")
+    plt.legend()
+    plt.grid(True)
+    plt.tight_layout()
+    plt.savefig( "timeXdist.png" , bbox_inches='tight' );
+    plt.clf()
 
 main()
